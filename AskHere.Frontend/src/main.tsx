73e594fac11ee706add5edbce2939c3f217fcbc7
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-12-29 13:23:35 +0800

    setup routes

--- AskHere.Frontend/src/main.tsx
+++ AskHere.Frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
+
+import HomePage from "./pages/HomePage.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
