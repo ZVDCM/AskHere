a922355bbca4b8d1116422a0967107b32744e9cf
Zehrei Verj <moraleszehreiverj8@gmail.com>
2023-12-29 13:16:06 +0800

    setup frontend project structure

---
+++ AskHere.Frontend/src/components/layouts/AnswersLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AnswersLayout() {
+	return <div>AnswersLayout</div>;
+}
+
+export default AnswersLayout;
---
+++ AskHere.Frontend/src/components/layouts/QuestionsLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function QuestionsLayout() {
+	return <div>QuestionsLayout</div>;
+}
+
+export default QuestionsLayout;
